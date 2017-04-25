0c05dbbf1f783ddec4964330df0b446095d578f6
jennifergates <jennifergates@users.noreply.github.com>
2017-04-25 21:24:34 +0200

    separated out inotify install in case it fails

--- ossec_install_linux_agents_en_masse-by_installer.sh
+++ ossec_install_linux_agents_en_masse-by_installer.sh
@@ -24,19 +24,20 @@ else
 		echo "${USERNAME}@${HOST} with $INSTALLER"	
 		echo "________________________________"
 		echo "Using ${USERNAME} and ${KEY}, to connect to ${HOST} and install the ossec agent from ${TAR}."
-		echo "SSH key passphrase and user password will need to be entered for each command."
+		echo "SSH key passphrase and user password may need to be entered for each command."
 		echo ""
-		echo "[ ] Copying ${TAR} to host. Prompts for ssh key passphrase."
+		echo "[ ] Copying ${TAR} to host. May prompt for ssh key passphrase."
 		scp -i $KEY $TAR ${USERNAME}@${HOST}:/tmp/ossec-hids-LS17-2.8.3.tar.gz
 	
-		echo "[ ] Extracting ${TAR} to /tmp/ on ${HOST}. Prompts for ssh key passphrase."
+		echo "[ ] Extracting ${TAR} to /tmp/ on ${HOST}. May prompt for ssh key passphrase."
 		ssh -i $KEY ${USERNAME}@${HOST} 'tar -zxf /tmp/ossec-hids-LS17-2.8.3.tar.gz -C /tmp/'
 	
 		if [ $INSTALLER = "apt" ] || [ $INSTALLER = "APT" ] || [ $INSTALLER = "Apt" ]; then
 			echo "[ ] Installing prerequisites with apt and running unattended install. Prompts for ssh key passphrase and user's password for sudo command."
-			ssh -i $KEY ${USERNAME}@${HOST} -tt 'export DEBIAN_FRONTEND=noninteractive && apt-get install build-essential libssl-dev inotify-tools --assume-yes && cd /tmp/ossec-hids-2.8.3 && ./install.sh && /var/ossec/bin/agent-auth -m 10.11.5.2 -p 1515 && /var/ossec/bin/ossec-control restart'
+			ssh -i $KEY ${USERNAME}@${HOST} -tt 'export DEBIAN_FRONTEND=noninteractive && apt-get install --assume-yes'
+			ssh -i $KEY ${USERNAME}@${HOST} -tt 'export DEBIAN_FRONTEND=noninteractive && apt-get install build-essential libssl-dev --assume-yes && cd /tmp/ossec-hids-2.8.3 && ./install.sh && /var/ossec/bin/agent-auth -m 10.11.5.2 -p 1515 && /var/ossec/bin/ossec-control restart'
 		
-		else
+		else 
 			echo "[ ] Installing prerequisites with yum and running unattended install. Prompts for ssh key passphrase and user's password for sudo command."	
 			ssh -i $KEY ${USERNAME}@${HOST} -tt 'export DEBIAN_FRONTEND=noninteractive && yum install -y openssl-devel && cd /tmp/ossec-hids-2.8.3 && ./install.sh && /var/ossec/bin/agent-auth -m 10.11.5.2 -p 1515 && /var/ossec/bin/ossec-control restart'
 		fi
