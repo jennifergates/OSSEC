f659259f793ae5c7865965a6b707bd5f6a2c1198
jennifergates <jennifergates@users.noreply.github.com>
2017-04-25 18:38:08 +0000

    works properly now and restarts service

--- ossec_remove_all_agents.sh
+++ ossec_remove_all_agents.sh
@@ -2,7 +2,8 @@
 ######
 # This Script runs a list command on the existing clients added to the OSSEC server
 # and then removes them, one by one, using the manage_agents CLI API and the agent
-# IDs pulled from the list output. Run this on the server with root privileges.
+# IDs pulled from the list output. If successfully removes an agent, it restarts the
+# OSSEC server service.  Run this on the server with root privileges.
 ######
 
 clear
@@ -14,13 +15,26 @@ fi
 
 echo "This script will remove all agents currently added to this OSSEC server."
 echo "Type "Remove" to proceed."
-read $Continue
+read Continue
 
-if [ $Continue -eq "Remove"]; then 
-	var/ossec/bin/manage_agents -l | awk '{print $2}' | sed 's/,$//' > /tmp/ids
+if [ $Continue = "Remove" ] ; then 
+	/var/ossec/bin/manage_agents -l | awk '{print $2}' | sed 's/,$//' | sed '/^\s*$/d' > /tmp/ids
 	while read -r var; do 
-		echo removing $var;
-		sudo /var/ossec/bin/manage_agents -r $var; 
-	done < /tmp/ids;
+		if [ $var = "agents:" ]; then
+			continue;
+		fi
+		if [ $var = "No" ]; then
+			echo "No Agents to Remove";
+			exit 0;
+		else
+			echo "Removing $var";
+			/var/ossec/bin/manage_agents -r $var;
+		fi 
+	done < /tmp/ids ;
 fi
-	
+
+echo "Restarting the OSSEC service"
+/var/ossec/bin/ossec-control restart
+
+#/var/ossec/bin/manage_agents -l
+rm /tmp/ids
